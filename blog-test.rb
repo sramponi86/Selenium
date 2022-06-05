9174e439f9c3858d8b3d05d3a994a462bf25f25e
sramponi86 <46656547+sramponi86@users.noreply.github.com>
2022-06-05 15:07:45 +0200

    Add files via upload

---
+++ blog-test.rb
@@ -0,0 +1,45 @@
+require "selenium-webdriver"
+require "rspec"
+timestamp = Time.now.to_i
+user = "ramse#{timestamp}"
+mail = "ramse#{timestamp}@test.com"
+pass = "1234"
+
+def enter_username(u_name)
+	username = driver.find_element(id: "user_username")
+	username.send_keys(u_name)
+end
+
+def enter_mail(e_mail)
+	email = driver.find_element(id: "user_email")
+	email.send_keys(e_mail)
+end
+
+def enter_password(password)
+	password = driver.find_element(id: "user_password")
+	password.send_keys(password)
+end
+# TEST: Sign up for blog
+describe "Blog application" do 
+	describe "singup to the blog application" do
+		it "confirm that a user successfully singup" do
+			driver = Selenium::WebDriver.for :firefox
+			driver.navigate.to "https://selenium-blog.herokuapp.com/signup"
+			enter_username(user)
+
+			enter_mail(mail)
+
+			enter_password(pass)
+			
+			signup = driver.find_element(id: "submit")
+
+			signup.click
+
+			banner = driver.find_element(id: "flash_success")
+			b_text = banner.text
+			expect(b_text).to eq("Welcome to the alpha blog ramse3")
+
+			driver.quit
+		end
+	end
+end
