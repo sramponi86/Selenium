6f3a41c17811da32f42094c0215962e5216c6788
sramponi86 <46656547+sramponi86@users.noreply.github.com>
2022-06-05 14:58:57 +0200

    Update blog-test_grid.rb

--- blog-test_grid.rb
+++ blog-test_grid.rb
@@ -1,6 +1,9 @@
 require "selenium-webdriver"
 require "rspec"
-
+timestamp = Time.now.to_i
+user = "ramse#{timestamp}"
+mail = "ramse#{timestamp}@test.com"
+pass = "1234"
 # TEST: Sign up for blog
 describe "Blog application" do 
 	describe "singup to the blog application" do
@@ -8,11 +11,11 @@ describe "Blog application" do
 			driver = Selenium::WebDriver.for :remote, desired_capabilities: :firefox
 			driver.navigate.to "https://selenium-blog.herokuapp.com/signup"
 			username = driver.find_element(id: "user_username")
-			username.send_keys("ramse3")
+			username.send_keys(user)
 			email = driver.find_element(id: "user_email")
-			email.send_keys("ramse89@test.com")
+			email.send_keys(mail)
 			password = driver.find_element(id: "user_password")
-			password.send_keys("1234")
+			password.send_keys(pass)
 			signup = driver.find_element(id: "submit")
 
 			signup.click
